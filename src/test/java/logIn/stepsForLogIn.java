3ec48f25e7a6bbea11d1d4fbd6b17d7f69a4c376
tothbenceimre <benceimre.toth@gmail.com>
2021-03-16 21:41:36 +0100

    stepsForLogIn.java added to the project

---
+++ src/test/java/logIn/stepsForLogIn.java
@@ -0,0 +1,47 @@
+package logIn;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepsForLogIn {
+    @Given("we have a Jira user registered.")
+    public void weHaveAJiraUserRegistered() {
+
+    }
+
+    @And("we navigated to the dashboard page.")
+    public void weNavigatedToTheDashboardPage() {
+
+    }
+
+    @And("we click on {string} button on the top right corner.")
+    public void weClickOnButtonOnTheTopRightCorner(String button) {
+
+    }
+
+    @When("we fill the username field with our username.")
+    public void weFillTheUsernameFieldWithOurUsername() {
+
+    }
+
+    @And("we fill the password field with our username's password.")
+    public void weFillThePasswordFieldWithOurUsernameSPassword() {
+
+    }
+
+    @And("we click on {string} button.")
+    public void weClickOnButton(String button) {
+
+    }
+
+    @Then("we are no longer able to see the {string} button on the top right.")
+    public void weAreNoLongerAbleToSeeTheButtonOnTheTopRight(String button) {
+
+    }
+
+    @And("we can click on our avatar on the top right.")
+    public void weCanClickOnOurAvatarOnTheTopRight() {
+    }
+}
