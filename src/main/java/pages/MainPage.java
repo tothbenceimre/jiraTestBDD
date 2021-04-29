dbfc85f9a2a42b285636024d4686504ba355ccc4
tothbenceimre <benceimre.toth@gmail.com>
2021-04-29 19:32:03 +0200

    refactor for MainPage.java

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,30 +1,26 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import util.Waiter;
 
 public class MainPage {
     WebDriver driver;
+    Waiter waiter;
 
     @FindBy(xpath = "//*[@id=\"header-details-user-fullname\"]/span/span/img") WebElement avatar;
     @FindBy(id = "view_profile") WebElement viewProfileTab;
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
+        this.waiter = new Waiter(this.driver);
         PageFactory.initElements(this.driver, this);
     }
 
-    public void loadPage(){
-        new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOf(avatar));
-    }
-
-
     public void navigateToProfile () {
+        this.waiter.waitForElementToAppear(avatar);
         avatar.click();
         viewProfileTab.click();
     }
