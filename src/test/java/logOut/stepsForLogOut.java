3f7425bb089f136f048f4d5ca0c2d770e80f6a4f
tothbenceimre <benceimre.toth@gmail.com>
2021-05-10 19:52:14 +0200

    finalized steps for stepsForLogOut.java

--- src/test/java/logOut/stepsForLogOut.java
+++ src/test/java/logOut/stepsForLogOut.java
@@ -1,37 +1,47 @@
 package logOut;
 
+import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.DashboardPage;
+import pages.MainPage;
+import util.Driver;
 
 public class stepsForLogOut {
+    Driver driver = new Driver();
+    DashboardPage dashboardPage = new DashboardPage(driver.getDriver());
+    MainPage mainPage = new MainPage(driver.getDriver());
+
     @Given("that we are logged in.")
     public void thatWeAreLoggedIn() {
-
+        driver.navigation(driver.getBaseUrl());
+        dashboardPage.login(System.getenv("jirausername"), System.getenv("jirapassword"));
     }
 
     @And("we can see our avatar.")
     public void weCanSeeOurAvatar() {
-
+        mainPage.avatarIsVisible();
     }
 
     @When("we click on our avatar.")
     public void weClickOnOurAvatar() {
-
+        mainPage.clickOnAvatar();
     }
 
-    @And("we click on {string}.")
-    public void weClickOn(String button) {
-
+    @And("we click on Log out.")
+    public void weClickOnLogOut() {
+        mainPage.clickOnLogOutTab();
     }
 
-    @Then("we no longer able to see our avatar.")
-    public void weNoLongerAbleToSeeOurAvatar() {
-
+    @Then("we get back a message that says: You're now logged out..")
+    public void weGetBackAMessageThatSaysYouReNowLoggedOut() {
+        dashboardPage.verifySuccessfulLogOut();
     }
 
-    @And("we get back a message that says: {string}.")
-    public void weGetBackAMessageThatSays(String message) {
+    @After
+    public void tearDown () {
+        driver.close();
     }
 }
