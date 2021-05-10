f444fdbdb60eaa8e19f89b9e3447e640e27355b6
tothbenceimre <benceimre.toth@gmail.com>
2021-05-10 20:10:56 +0200

    removed unnecessary imports from Driver.java

--- src/main/java/util/Driver.java
+++ src/main/java/util/Driver.java
@@ -1,10 +1,7 @@
 package util;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.concurrent.TimeUnit;
 
 public class Driver {
     private WebDriver driver;
