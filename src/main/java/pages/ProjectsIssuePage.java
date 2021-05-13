117128958594cfbf5472f9a73d9387d574fd8d68
tothbenceimre <benceimre.toth@gmail.com>
2021-05-13 10:05:52 +0200

    added clickOnCreateButton method for ProjectsIssuePage.java

--- src/main/java/pages/ProjectsIssuePage.java
+++ src/main/java/pages/ProjectsIssuePage.java
@@ -1,19 +1,30 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import util.Waiter;
 
 public class ProjectsIssuePage {
     WebDriver driver;
+    Waiter waiter;
 
     @FindBy(xpath = "//*[@class='inline-issue-create-container']//button[contains(., 'Create issue')]") WebElement createButton;
     @FindBy(xpath = "//form[@class='iic-widget__form aui']//textarea") WebElement summaryField;
+    @FindBy(xpath = "//*[@class='iic-widget__issue-type-selector']//button") WebElement issueTypeField;
+    @FindBy(id = "edit-issue") WebElement editIssueButton;
 
 
     public ProjectsIssuePage(WebDriver driver) {
         this.driver = driver;
+        this.waiter = new Waiter(this.driver);
         PageFactory.initElements(this.driver, this);
     }
+
+    public void clickOnCreateButton () { createButton.click(); }
+
+
 }
